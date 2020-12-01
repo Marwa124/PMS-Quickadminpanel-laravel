4ebb6f9961a2569118977451b6e8aaef017ac5ce
saraadelalshora <saraadelalshora@yahoo.com>
2020-12-01 13:47:45 +0200

    work with sales moduls

--- Modules/HR/Database/Migrations/2020_09_27_000146_add_relationship_fields_to_meeting_minutes_table.php
+++ Modules/HR/Database/Migrations/2020_09_27_000146_add_relationship_fields_to_meeting_minutes_table.php
@@ -9,7 +9,7 @@ class AddRelationshipFieldsToMeetingMinutesTable extends Migration
     public function up()
     {
         Schema::table('meeting_minutes', function (Blueprint $table) {
-            $table->unsignedInteger('user_id');
+            // $table->unsignedInteger('user_id');
             $table->foreign('user_id', 'user_fk_2167949')->references('id')->on('users');
         });
     }
---
+++ Modules/Sales/Routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::prefix('sales')->group(function() {
+//     Route::get('/', 'SalesController@index');
+// });
+Route::group(['as' => 'sales.admin.', 'prefix' => 'admin/sales', 'namespace' => 'Admin', 'middleware' => ['auth']],function() {
+    Route::get('/', 'SalesController@index');
+     // Proposals
+    Route::delete('proposals/destroy', 'ProposalsController@massDestroy')->name('proposals.massDestroy');
+    Route::post('proposals/media', 'ProposalsController@storeMedia')->name('proposals.storeMedia');
+    Route::post('proposals/ckmedia', 'ProposalsController@storeCKEditorImages')->name('proposals.storeCKEditorImages');
+    Route::resource('proposals', 'ProposalsController');
+
+    // Proposals Items
+    Route::delete('proposals-items/destroy', 'ProposalsItemsController@massDestroy')->name('proposals-items.massDestroy');
+    Route::post('proposals-items/media', 'ProposalsItemsController@storeMedia')->name('proposals-items.storeMedia');
+    Route::post('proposals-items/ckmedia', 'ProposalsItemsController@storeCKEditorImages')->name('proposals-items.storeCKEditorImages');
+    Route::resource('proposals-items', 'ProposalsItemsController');
+
+    // Interested Ins
+    Route::delete('interested-ins/destroy', 'InterestedInController@massDestroy')->name('interested-ins.massDestroy');
+    Route::resource('interested-ins', 'InterestedInController', ['except' => ['edit', 'update', 'show']]);
+
+});
